503e17f062f0ba208d4c9a54a89634fa0121a255
00seungho <tmdgh9910@naver.com>
2023-05-31 00:47:47 +0900

    굿

--- controllers/v1/common-controller.js
+++ controllers/v1/common-controller.js
@@ -1,12 +1,12 @@
 const { url } = require('inspector');
 const CommonService = require('../../services/v1/common-service');
 const RegisterService = require('../../services/v1/register-service');
+const axios = require('axios');
+
 
 exports.Con_get_bus = async (req, res, next) => {
     try {
         
-        const axios = require('axios');
-
         let config = {
           method: 'get',
           maxBodyLength: Infinity,
@@ -34,9 +34,8 @@ exports.Con_get_bus = async (req, res, next) => {
 
 exports.Con_sub_master = async (req, res, next) => {
     try {
-        var startIndex=1;
-        var endIndex=999;
 
+        
         var url = `http://openapi.seoul.go.kr:8088/74776a5341746d6439394a57735854/json/SeoulMetroFaciInfo/${startIndex}/${endIndex}/`
         
         const axios = require('axios');
@@ -49,9 +48,9 @@ exports.Con_sub_master = async (req, res, next) => {
         
         axios.request(config)
         .then((response) => {
-            console.log(typeof(response));
-            response.SeoulMetroFaciInfo.list_total_count
-        })
+ 
+
+          })
         .catch((error) => {
           console.log(error);
         });
@@ -66,7 +65,6 @@ exports.Con_sub_master = async (req, res, next) => {
 exports. Con_get_sub_ev = async (req, res, next) => {
     try {
         
-        const axios = require('axios');
 
         let config = {
           method: 'get',
