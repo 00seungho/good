5502002a24d7c3fe3c5271d47baa2a5b62ac747d
00seungho <tmdgh9910@naver.com>
2023-05-31 00:37:21 +0900

    gdgd

--- controllers/v1/common-controller.js
+++ controllers/v1/common-controller.js
@@ -1,3 +1,4 @@
+const { url } = require('inspector');
 const CommonService = require('../../services/v1/common-service');
 const RegisterService = require('../../services/v1/register-service');
 
@@ -33,22 +34,23 @@ exports.Con_getBusMap = async (req, res, next) => {
 
 exports. Con_get_bus = async (req, res, next) => {
     try {
+        var startIndex=1;
+        var endIndex=999;
+
+        var url = `http://openapi.seoul.go.kr:8088/74776a5341746d6439394a57735854/json/SeoulMetroFaciInfo/${startIndex}/${endIndex}/`
         
         const axios = require('axios');
-
         let config = {
           method: 'get',
           maxBodyLength: Infinity,
-          url: 'http://openapi.seoul.go.kr:8088/74776a5341746d6439394a57735854/json/subwayStationMaster/1/999/',
+          url: url,
           headers: {}
         };
         
         axios.request(config)
         .then((response) => {
-          console.log(JSON.stringify(response.data));
-            return res.status(200).json({
-                good:response.data
-        })
+            console.log(typeof(response));
+            response.SeoulMetroFaciInfo.list_total_count
         })
         .catch((error) => {
           console.log(error);
